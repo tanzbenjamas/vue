4eee490b3bb1baa5c69390f94c7330b768aac2dd
Your Name <you@example.com>
2019-10-07 18:29:35 +0700

    client

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app style = "background: #f5f5f5;">
+    
+    <v-app-bar style = "background: white;" app>
+      <v-toolbar-title  class="headline text-uppercase" >
+        <span class="font-weight-light">WELCOME</span>        
+      </v-toolbar-title>
+      
+</v-app-bar>        
+<br>
+<br>
+<br>
+<br>
+    <v-content>   
+     
+     
+     
+      <router-view/> 
+    </v-content>
+  </v-app>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    
+  }),
+};
+</script>
