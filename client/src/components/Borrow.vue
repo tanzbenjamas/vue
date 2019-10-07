1bc90347ec4fb15059c91f6795fde2f17e8c5415
tanzbenjamas <45569862+tanzbenjamas@users.noreply.github.com>
2019-10-08 00:45:54 +0700

    add edit Borrow.vue

--- client/src/components/Borrow.vue
+++ client/src/components/Borrow.vue
@@ -1,13 +1,11 @@
 <template>
   <v-container>
     
-        <v-layout text-center wrap>
+   <v-layout text-center wrap>
       <v-flex mb-4>
-     
         <h1 style="margin-top: -80px;"  class="display-2 font-weight-bold mb-3">แบบฟอร์มยืมอุปกรณ์กีฬา</h1>
-      
-      </v-flex>
-    </v-layout>
+          </v-flex>
+           </v-layout>
 
     
     <v-row justify="center">
@@ -31,10 +29,10 @@
           </v-row>
          
     
-     <v-row justify="center"> 
-        <!-- อุปกรณ์กีฬา -->
-            <v-col cols="10">
-              <v-select
+            <v-row justify="center"> 
+              <!-- อุปกรณ์กีฬา -->
+                <v-col cols="10">
+                <v-select
                 label="อุปกรณ์กีฬา"
                 solo
                 v-model="Borrow.sportEquipment"
@@ -65,85 +63,82 @@
                 prepend-icon="mdi mdi-settings"
               ></v-select>
             </v-col>
-         </v-row>
+        </v-row>
 
-    <v-row justify="center">  
-        <!-- จำนวนอุปกรณ์ -->
-            <v-col cols="10">
-              <v-text-field
-                solo  
-                label="จำนวนอุปกรณ์"
-                v-model= "Borrow.numberequipment"
+            <v-row justify="center">  
+                <!-- จำนวนอุปกรณ์ -->
+                  <v-col cols="10">
+                    <v-text-field
+                   solo  
+                    label="จำนวนอุปกรณ์"
+                    v-model= "Borrow.numberequipment"
               
-                :rules="[(v) => !!v || 'กรุณากรอกข้อมูล']"
-                required
+                    :rules="[(v) => !!v || 'กรุณากรอกข้อมูล']"
+                     required
                 
-               prepend-icon= "mdi mdi-calculator"
-              ></v-text-field>
-            </v-col>
-          
-  <v-col cols="10">
-      <v-menu
-        v-model="menu2"
-        :close-on-content-click="false"
-        :nudge-right="40"
-        transition="scale-transition"
-        offset-y
-        min-width="290px"
-      >
-        <template v-slot:activator="{ on }">
-          <v-text-field
-            v-model="date"
-              solo  
-            label="Picker without buttons"
-            prepend-icon= "mdi mdi-calendar"
+                     prepend-icon= "mdi mdi-calculator"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+          <v-row justify="center"> 
+                <v-col cols="10">
+                 <v-menu
+                 v-model="menu2"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                 transition="scale-transition"
+                 offset-y
+                min-width="290px"
+                 >
+                <template v-slot:activator="{ on }">
+                 <v-text-field
+                 
+                solo  
+                label="กรุณาเลือกวันที่"
+                v-model="Borrow.date"
+                 :rules="[(v) => !!v || 'กรุณาเลือกวันที่']"
+                  required
+                prepend-icon= "mdi mdi-calendar"
            
-            readonly
-            v-on="on"
-          ></v-text-field>
-        </template>
-        <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
-      </v-menu>
-     </v-col>
-    
-
- 
-        <!-- พนักงาน -->
-            <v-col cols="10">
-              <v-select
-                label="พนังงาน"
-                solo
-                v-model="Borrow.employee"
-                :items="employee"
-                item-text="employeename"
-                item-value="employeeid"
-                :rules="[(v) => !!v || 'กรุณากรอกเลือกข้อมูล']"
-                required
-                style="margin-top: -30px;"
-                prepend-icon="mdi mdi-account-multiple-outline"
-              ></v-select>
-             </v-col>
+                             readonly
+                          v-on="on"
+                        ></v-text-field>
+                     </template>
+                   <v-date-picker v-model="Borrow.date" @input="menu2 = false"></v-date-picker>
+               </v-menu>
+            </v-col>
           </v-row>
 
+             <v-row justify="center"> 
+              <!-- พนักงาน -->
+               <v-col cols="10">
+                  <v-select
+                  label="พนังงาน"
+                  solo
+                  v-model="Borrow.employee"
+                  :items="employee"
+                  item-text="employeename"
+                  item-value="employeeid"
+                  :rules="[(v) => !!v || 'กรุณากรอกเลือกข้อมูล']"
+                  required
+                  style="margin-top: -30px;"
+                 prepend-icon="mdi mdi-account-multiple-outline"
+                ></v-select>
+               </v-col>
+            </v-row>
 
-          <v-row justify="center">
-            <v-col cols="15">
-              <v-btn 
-                style="margin-left: 50%;"
-                @click="saveBorrow">บันทึก</v-btn>
-                 </v-col>
-          
-      
-            <v-col cols="15">
-              <v-btn 
-                style="margin-right: 50%;"
-                @click="clear">ยกเลิก</v-btn>
-                
-                 </v-col>
-          </v-row>
-        </v-form>
-      </v-col>
-    </v-row>
+
+              <v-row justify="center">
+                <v-col cols="15">
+                <v-btn  style="margin-left: 90px;" @click="saveBorrow">บันทึก</v-btn> 
+                <v-btn @click="clear" color="#000000" style="margin-left: 100px; color:#FFFFFF">ยกเลิก</v-btn>
+                </v-col>
+              </v-row>
+        
+            
+           </v-form>
+         </v-col>
+      </v-row>
   </v-container>
 </template>
 
@@ -156,26 +151,23 @@ export default {
   data() {
     return {
       Borrow: {
-        employee:null,
-        sportEquipment: null,
-        typeEquipment: null,
-     },
-        nameborrow : '',
-        numberequipment: '',
        
+        employee: "",
+        sportEquipment: "",
+        typeEquipment: "",
+     },
+      
+        nameborrow:"",
+        numberequipment:"",
         date: new Date().toISOString().substr(0, 10),
         menu2: false,
         employee: null,
         sportEquipment: null,
         typeEquipment: null,
-        
-       
-        };
+    }
   },
 
-
-
-  methods: {
+methods: {
     
      /* eslint-disable no-console */
 
@@ -215,14 +207,8 @@ export default {
           console.log(e);
         });
     },
-        // ดึงข้อมูล employee ใส่ combobox
-
   
-  ///Borrow/{nameborrow}/{numberequipment}/{empselect}/{eqmselect}/{typeeqmselect}
-
-
-
-    // function เมื่อกดปุ่ม save
+ // function เมื่อกดปุ่ม save
    saveBorrow() {
       http
         .post(
@@ -231,7 +217,7 @@ export default {
             "/" +
             this.Borrow.numberequipment +
             "/" + 
-            this.date +
+            this.Borrow.date +
             "/" +
             this.Borrow.employee +
             "/" +
@@ -253,7 +239,15 @@ export default {
       alert("บันทึกสำเร็จ")
       },
       clear(){
-            this.$router.push("/view");
+        //this.$refs.form.reset();
+        this.Borrow.nameborrow='';
+        this.Borrow.numberequipment='';
+        this.Borrow.date='';
+        this.menu2= '';
+        this.Borrow.employee= null;
+        this.Borrow.sportEquipment= null;
+        this.Borrow.typeEquipment= null;
+        
       },
 
     refreshList() {
@@ -264,11 +258,12 @@ export default {
     }
     /* eslint-enable no-console */
   },
+  
   mounted() {
     this.getemployee();
     this.getsportEquipment();
     this.gettypeEquipment();
     
-  }
-};
+  },
+}
 </script>
